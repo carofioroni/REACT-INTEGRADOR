dc7a2e12961f385f23631ce38cf1c82ddfb4392a
dafnemus <dafnemus@gmail.com>
2023-07-13 14:18:38 -0300

    completed searchBar component

--- proyecto-pokedex/src/components/SearchBar/index.jsx
+++ proyecto-pokedex/src/components/SearchBar/index.jsx
@@ -1,7 +1,8 @@
 import iconSearch from "../../assets/icons/search.svg";
+import iconClose from "../../assets/icons/close.svg";
 import "./styles.css";
 
-function SearchBar({ onChange, value }) {
+function SearchBar({ onChange, value, onClose }) {
   return (
     <div className="search-bar">
       <img className="" src={iconSearch} alt="lupa buscadora" />
@@ -11,6 +12,7 @@ function SearchBar({ onChange, value }) {
         placeholder="Search"
         value={value}
       />
+      {value && <img className="" src={iconClose} alt="lupa buscadora" onClick={onClose} />}
     </div>
   );
 }
