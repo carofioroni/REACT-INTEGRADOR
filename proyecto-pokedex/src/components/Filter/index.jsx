9c4c931c5a272a11c713a504c587be7cc2824e91
dafnemus <dafnemus@gmail.com>
2023-07-14 09:03:46 -0300

    modified Filter component

--- proyecto-pokedex/src/components/Filter/index.jsx
+++ proyecto-pokedex/src/components/Filter/index.jsx
@@ -2,10 +2,10 @@ import letter from "../../assets/icons/text_format.svg";
 import hashtag from "../../assets/icons/tag.svg";
 import "./styles.css";
 
-function Filter({ onClick, value }) {
+function Filter({ onClick, typeFilter }) {
   return (
     <a className="containerFilter" onClick={onClick}>
-      {value ? (
+      {typeFilter ? (
         <img src={letter} alt="alfabeto icon" />
       ) : (
         <img src={hashtag} alt="numeral icon" />
