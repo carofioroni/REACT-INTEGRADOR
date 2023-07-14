09f1cfee629bebe04bfe742723a617bf083261cd
carofioroni <carofioroni@gmail.com>
2023-07-14 16:03:28 -0300

    llamada a la api desde app.jsx

--- proyecto-pokedex/src/App.jsx
+++ proyecto-pokedex/src/App.jsx
@@ -3,16 +3,29 @@ import "./App.css";
 import Filter from "./components/Filter";
 import PokemonCard from "./components/Card";
 import iconSearch from "./assets/icons/search.svg";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BigCard from './components/big-card/index.jsx'
 
 function App() {
   const [id, setId] = useState(1)
+  const [pokemons, setPokemons]= useState([])
+  useEffect(() => { 
+    const getPokemons = async () => {
+      const httpRequest = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20"); 
+      const respuesta = await httpRequest.json();
+      setPokemons(respuesta.results);
+    };
+
+    getPokemons();
+
+  }, []);
+  console.log(pokemons)
   return (
     <>
       <SearchBar />
       <Filter />
-      <PokemonCard order="001" name="pokemon" img={iconSearch} />
+      {pokemons.map(pokemon => <PokemonCard order={pokemons[pokemon + 1]} name={pokemon.name} img={iconSearch} /> ) }
+      
       <BigCard id={id}/>
     </>
   );
