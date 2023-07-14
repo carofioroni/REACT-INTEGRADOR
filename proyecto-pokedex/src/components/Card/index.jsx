b667f71ef13bbbcd72a3959c8132cc916f26081d
dafnemus <dafnemus@gmail.com>
2023-07-14 16:40:30 -0300

    resolve bug styles

--- proyecto-pokedex/src/components/Card/index.jsx
+++ proyecto-pokedex/src/components/Card/index.jsx
@@ -1,10 +1,10 @@
 import "./styles.css";
 function PokemonCard({ order, name, img, openModal }) {
   return (
-    <div onClick={openModal} className="card">
-      <p className="order">#{order}</p>
-      <img className="image" src={img} alt="pokemon" />
-      <h2 className="name">{name}</h2>
+    <div onClick={openModal} className="smallCard">
+      <p className="orderPoke">#{order}</p>
+      <img className="imagePoke" src={img} alt="pokemon" />
+      <h2 className="namePoke">{name}</h2>
     </div>
   );
 }
